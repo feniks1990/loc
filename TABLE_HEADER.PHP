1e475f034c39b182ae02e049349106b95eb4aabf
feniks1990 <m_a_a_90@mail.ru>
2017-09-13 11:58:48 +0300

    Create TABLE_HEADER.PHP

---
+++ TABLE_HEADER.PHP
@@ -0,0 +1,34 @@
+<tr>
+								<th>
+									<div class="filter" data-dropdown>
+										<i class="icon icon-filter anchor"></i>
+										<div class="dropdown">
+											<ul class="clear">
+												<li data-filter="" data-col="0" class="active">Показать все</li>
+											</ul>
+										</div>
+									</div>
+									Номер заказа
+								</th>
+								<th>Контракт</th>
+								<th>Номер заявки покупателя</th>
+								<th>
+									<div class="filter" data-dropdown>
+										<i class="icon icon-filter anchor"></i>
+										<div class="dropdown">
+											<ul class="clear">
+												<li data-filter="" data-col="3" class="active">Показать все</li>
+											</ul>
+										</div>
+									</div>
+									Дата заявки покупателя
+								</th>
+								<th>Сплав</th>
+								<th>Номенклатура</th>
+								<th>Торговый статус</th>
+								<th>Фонд, кг</th>
+								<th>Сдано, кг</th>
+								<th>Склад, кг</th>
+								<th>Согласованная дата сдачи</th>
+								<th>Дата деаллокации</th>
+							</tr>
